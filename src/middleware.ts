a463a405d3da15e45244aeb9274291d47add0332
avinash <avinashsharma17012002@gmail.com>
2024-12-05 21:41:31 +0530

    auth setup

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,32 +1,27 @@
-import {NextRequest,NextResponse} from 'next/server'
-import { NextApiRequest } from 'next'
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import { getToken } from "next-auth/jwt";
 
-export {default} from "next-auth/middleware"
 
-import { getToken } from 'next-auth/jwt'
+
 
 export async function middleware(request: NextRequest) {
 
-
-
  const token = await getToken({req: request})
+
  const url = request.nextUrl
 
+ if (token && (
+  url.pathname.startsWith('/sign-in') || url.pathname.startsWith('/sign-up') || url.pathname.startsWith('/verify') || url.pathname.startsWith('/') 
+ )) {
+  return NextResponse.redirect('/sign-in') 
 
- if (token && (url.pathname.startsWith('/sign-in') || url.pathname.startsWith('/sign-in') || url.pathname.startsWith('/sign-up') || url.pathname.startsWith('/verify') || url.pathname.startsWith('/'))) {
-  return NextResponse.redirect(new URL('/dashboard', request.url))
  }
-
+ 
  return NextResponse.redirect(new URL('/home', request.url))
-
 }
 
+
 export const config = {
- matcher : [
-  '/sign-in',
-  '/sign-up',
-  '/',
-  '/dashboard/:path',
-  '/verify/:path*'
- ]
+ matcher: ['/sign-in', 'sign-up', '/', 'dashboard/: path*', '/verify/:path*'],
 }
